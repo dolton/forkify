c28d4f97162ba0a34dbfdf5e6c1d0edf825ccbfb
Dsilva <doltondsilva23@gmail.com>
2019-01-31 16:28:09 +0530

    Modified the package json for new versions

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,30 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: ['babel-polyfill', './src/js/index.js'],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'js/bundle.js'
+    entry : [
+        'babel-polyfill', 
+        './src/js/index.js'
+    ],
+    output : {
+        path : path.resolve(__dirname, 'dist/js'),
+        filename : 'bundle.js'
     },
-    devServer: {
-        contentBase: './dist'
+    devServer : {
+        contentBase : './dist'
     },
-    plugins: [
+    plugins : [
         new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: './src/index.html'
+            filename : 'index.html',
+            template : './src/index.html'
         })
     ],
-    module: {
-        rules: [
+    module : {
+        rules : [
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
+                test : /\.js$/,
+                exclude : /node_modules/,
+                use : {
+                    loader : 'babel-loader'
                 }
             }
         ]
